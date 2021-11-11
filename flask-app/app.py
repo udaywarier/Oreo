973ec1c0c8b99583ef2be91cfbd0368473f59b61
udaywarier <uday.warier1@gmail.com>
2021-11-10 23:17:00 -0800

    Cycle through all pictures on a 1 second internval

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template
+import os
+
 app = Flask(__name__)
 
+images = os.listdir('./static/')
+cache = {'idx': 0}
+
 @app.route("/")
-def hello_world():
-    return render_template('index.html')
+def oreo():
+    img_name = images[cache['idx'] % len(images)]
+    increment()
+    return render_template('index.html', img_name=img_name)
+
+def increment():
+    cache['idx'] = cache['idx'] + 1
+    return 'Incremented cache!'
